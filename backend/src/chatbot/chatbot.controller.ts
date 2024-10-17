f67a86cdf27dc046d54bacb8b8962f6517f22f88
Veronica-Alfr <veagalves@gmail.com>
2024-10-16 21:31:38 -0300

    chore: configurações iniciais do projeto + inicio de configuração de receber msg chatbot

---
+++ backend/src/chatbot/chatbot.controller.ts
@@ -0,0 +1,12 @@
+import { ChatbotService } from './chatbot.service';
+import { Body, Controller, Post } from '@nestjs/common';
+
+@Controller('chatbot')
+export class ChatbotController {
+  constructor(private readonly chatbotService: ChatbotService) {}
+
+  @Post('send-message')
+  async sendMessage(@Body('to') to: string, @Body('content') content: string) {
+    return this.chatbotService.sendMessage(to, content);
+  }
+}
---
+++ backend/src/chatbot/chatbot.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@nestjs/common';
+import axios from 'axios';
+import { v4 as uuidv4 } from 'uuid';
+import 'dotenv/config';
+
+@Injectable()
+export class ChatbotService {
+  private readonly apiUrl = `https://${process.env.CONTRACT_ID}.http.msging.net/messages`;
+  private readonly headers = {
+    Authorization: `Key ${process.env.API_KEY}`,
+    'Content-Type': 'application/json',
+  };
+
+  async sendMessage(to: string, content: string) {
+    const data = {
+      id: uuidv4(),
+      to: to,
+      type: 'text/plain',
+      content: content,
+    };
+
+    try {
+      const response = await axios.post(this.apiUrl, data, {
+        headers: this.headers,
+      });
+      return response.data;
+    } catch (error) {
+      throw new Error(`Failed to send message: ${error.message}`);
+    }
+  }
+}
