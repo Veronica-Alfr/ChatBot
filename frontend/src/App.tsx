555a7bf118e2ad7814ea71bec023a1ab2a0a2ed6
Veronica-Alfr <veagalves@gmail.com>
2025-01-31 22:27:58 -0300

    chore: removido arquivos/códigos não mais necessários

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,16 +2,9 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import Login from "./pages/Login";
 import Contacts from "./pages/Contacts";
-// import { useAuth } from "./utils/useAuth";
 // import Messages from "./pages/Message";
 
 const queryClient = new QueryClient();
-
-// const ProtectedRoute = ({ element }: { element: JSX.Element }) => {
-//   const { isAuthenticated } = useAuth();
-
-//   return isAuthenticated ? element : <Navigate to="/login" />;
-// };
 
 function App() {
   return (
